1b4496df32b09f4bc7e2603586c6fc4239cb4f04
Mohamed Hassan <152076722+MOHasssan12@users.noreply.github.com>
2024-07-06 15:23:59 +0300

    Update calc.c

--- makefile/src/calc.c
+++ makefile/src/calc.c
@@ -1,1 +1,15 @@
+int calc(char symbol , int var1 , int var2)
+{
+    int result ;
 
+    switch (symbol) 
+    {
+        case '+': result=var1+var2; break;
+        case '*': result=var1*var2; break;
+        case '-': result=var1-var2; break;
+        case '/': result=var1/var2; break;
+        default: break;
+    }
+
+    return result;
+}
