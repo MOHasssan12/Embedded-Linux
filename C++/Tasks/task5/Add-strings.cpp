a20a8459e119d9f2e935c8f906542bc1b46b559e
MOHasssan12 <eng.mohamedhassan844@gmail.com>
2024-07-21 15:07:42 +0300

    add files

---
+++ C++/Tasks/task5/Add-strings.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+
+class Solution {
+public:
+  std::string addStrings(std::string num1, std::string num2) {
+    std::string result = "";
+    int carry = 0;
+    int i = num1.size() - 1;
+    int j = num2.size() - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+      int n1 = (i >= 0) ? num1[i--] - '0' : 0;
+      int n2 = (j >= 0) ? num2[j--] - '0' : 0;
+      int sum = n1 + n2 + carry;
+      carry = sum / 10;
+      result += (sum % 10) + '0';
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
+  }
+};
---
+++ C++/Tasks/task5/Vaild-Anagram.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+
+class Solution {
+public:
+  bool isAnagram(std::string s, std::string t) {
+
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+
+    if (s == t) {
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+};
